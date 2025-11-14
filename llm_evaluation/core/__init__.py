b9374b701d3fa9a3d11b3bae865cefa0bbc96ba4
Trungneff <cauchubebong184@gmail.com>
2025-11-15 01:39:27 +0700

    remove from .prompt_builder  DEFAULT_EXAMPLES

--- llm_evaluation/core/__init__.py
+++ llm_evaluation/core/__init__.py
@@ -1,7 +1,7 @@
 from .checkpoint_manager import CheckpointManager
 from .evaluator import Evaluator
 from .model_interface import ModelInterface, get_model_interface, generate_text, clear_model_cache
-from .prompt_builder import create_prompt, DEFAULT_EXAMPLES
+from .prompt_builder import create_prompt
 from .result_analyzer import ResultAnalyzer
 from .reporting import Reporting
 
